d0c5265fe4f08de2194c88a0ad9cf4d6ed27182c
Gonzalo Garcia <gonzalo.garcia.92.gg@gmail.com>
2025-09-22 19:02:01 -0300

    simplificacion customobject

--- fragmento.js
+++ fragmento.js
@@ -1,19 +1,8 @@
 class MiComponente extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
-      <style>
-        .caja {
-          padding: 1em;
-          background: #e3e3e3;
-          border-radius: 8px;
-          font-family: sans-serif;
-        }
-      </style>
-      <div class="caja">
-        <p>Este es un componente web personalizado.</p>
-      </div>
+        <p>Gladiador II</p>
     `;
   }
 }
